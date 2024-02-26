fe8ef0c8494c1257d1c7bc459ba4f707c5e91b93
Zan Spiler <zan.spiler@celtra.com>
2024-02-26 18:51:25 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "e6876bbde18805252a28c1261a767fda",
-"index.html": "922b32a7f4521af9bc447f034267b67c",
-"/": "922b32a7f4521af9bc447f034267b67c",
-"main.dart.js": "eb66ccb520917fc182825deeeac898b2",
+"index.html": "b3cc292a781d038b067ebaa3031fc1fe",
+"/": "b3cc292a781d038b067ebaa3031fc1fe",
+"main.dart.js": "1545cb351ef98d966294982b8ed1037f",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
